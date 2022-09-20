5c27902f24acf2736b5f91014608ddf8badaf5b1
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-20 15:28:13 +0200

    Add readFromFile constructor

--- src/hu/petrik/EmberekOOP/Emberek.java
+++ src/hu/petrik/EmberekOOP/Emberek.java
@@ -1,127 +1,11 @@
 package hu.petrik.EmberekOOP;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Emberek {
-    private List<Ember> emberekList= new List<Ember>() {
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<Ember> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean add(Ember ember) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends Ember> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int index, Collection<? extends Ember> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public Ember get(int index) {
-            return null;
-        }
-
-        @Override
-        public Ember set(int index, Ember element) {
-            return null;
-        }
-
-        @Override
-        public void add(int index, Ember element) {
-
-        }
-
-        @Override
-        public Ember remove(int index) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public ListIterator<Ember> listIterator() {
-            return null;
-        }
-
-        @Override
-        public ListIterator<Ember> listIterator(int index) {
-            return null;
-        }
-
-        @Override
-        public List<Ember> subList(int fromIndex, int toIndex) {
-            return null;
-        }
-    };
+    private List<Ember> emberekList= new ArrayList<>();
 
     public Emberek(Ember emberObj) {
         emberekList.add(emberObj);
@@ -130,6 +14,15 @@ public class Emberek {
     public Emberek(Ember[] emberArr){
         for (int i = 0; i < emberArr.length; i++) {
             emberekList.add(emberArr[i]);
+        }
+    }
+
+    public Emberek (File file) throws IOException {
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()){
+            String[] temp =sc.nextLine().split(";");
+            Ember tempEmb = new Ember(temp[0],temp[1],temp[2]);
+            emberekList.add(tempEmb);
         }
     }
 
--- src/hu/petrik/EmberekOOP/Main.java
+++ src/hu/petrik/EmberekOOP/Main.java
@@ -1,9 +1,12 @@
 package hu.petrik.EmberekOOP;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Ember e1 = new Ember("Kiss Jóska", "2020.12.02", "Budapest");
         Ember e2 = new Ember("Kovács Balázs", "1990.03.02", "New York");
         Ember e3 = new Ember("Kiss László", "1989.04.04", "Paris");
@@ -16,9 +19,18 @@ public class Main {
         System.out.println(e3.birthDay());
         System.out.println(e1.currentAge());
 
-        Scanner sc = new Scanner(System.in);
-
         Emberek obj = new Emberek(new Ember[]{e1,e2,e3});
         System.out.println(obj);
+
+        File file = new File("emberek.txt");
+
+        try {
+            Emberek fromFile = new Emberek(file);
+        }catch (FileNotFoundException e){
+            System.out.printf("Hiba miatt nem található az %s fájl\n", file);
+        }catch (IOException e){
+            System.out.println("Ismeretlen hiba történt a fájl beolvasása során");
+        }
+
     }
 }
